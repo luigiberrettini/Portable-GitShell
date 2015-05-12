c9c2184d1c33de493247b6ad1400a9ed89506820
Luigi Berrettini <luigi.berrettini@gmail.com>
2015-05-12 20:31:28 +0200

    Add return to previous directory
    It can be used with Windows shortcuts "Start in" folder

--- New-GitShell.ps1
+++ New-GitShell.ps1
@@ -22,3 +22,5 @@ $env:posh_git = Resolve-Path $poshGitFolder
 . $env:posh_git\profile.example.ps1
 
 Register-EngineEvent -SourceIdentifier ([System.Management.Automation.PsEngineEvent]::Exiting) -Action { Stop-SshAgent } | Out-Null
+
+Pop-Location
